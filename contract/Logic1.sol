475d8db49b384f08e4b3b135a0f689d170b4d416
paopaolin <88531334+paopaolin@users.noreply.github.com>
2023-04-11 20:12:05 +0800

    Create Logic1.sol

---
+++ contract/Logic1.sol
@@ -0,0 +1,11 @@
+contract Logic1 {
+    // 状态变量和proxy合约一致，防止插槽冲突
+    address public implementation; 
+    address public admin; 
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    // 改变proxy中状态变量，选择器： 0xc2985578
+    function foo() public{
+        words = "old";
+    }
+}
